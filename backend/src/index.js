5e732206ae14bed7812c131a90a403ed8f00a7e6
Henok Gebresenbet <henokgebresenbet@Henoks-MacBook-Pro.local>
2025-08-04 14:38:01 +0300

    create book, implement middleware , upload image to cloudinary

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,12 @@ import { connectDB } from "./lib/db.js"
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 
-
-
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 
 app.use("/api/auth", authRoutes);
