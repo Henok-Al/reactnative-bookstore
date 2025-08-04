0992a1f4427535389c724247e8b6650d8a58a135
Henok Gebresenbet <henokgebresenbet@Henoks-MacBook-Pro.local>
2025-08-04 12:42:49 +0300

    register user

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,17 @@ import express from "express"
 import  "dotenv/config"
 import { connectDB } from "./lib/db.js"
 
+import authRoutes from "./routes/authRoutes.js";
+
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
+
 
 
 app.listen(PORT, () => {
