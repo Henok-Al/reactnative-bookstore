7dc77051a70ae4c92719c4ed4d2a31c5752f531a
Henok Gebresenbet <henokgebresenbet@Henoks-MacBook-Pro.local>
2025-08-04 23:06:55 +0300

    implment login page

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,24 +1,11 @@
-import { Image } from "expo-image";
-import { Text, View } from "react-native";
-
+import { Link } from "expo-router";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-    >
-      <Text style={{
-        color: "green"
-      }}>Hello world.</Text>
-      <Image source={{uri: "https://plus.unsplash.com/premium_photo-1751107029838-c341391ea8b2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"}}/>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Link href="/(auth)/signup">Signup</Link>
+      <Link href="/(auth)">Login</Link>
     </View>
   );
 }
-
-const styles = ({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff"
-  }
-})
